1a102c421631a3fafe2ac150e6f0567f2cd11ebc
Jan Piaskowy <piaskowyjasiek@gmail.com>
2021-10-21 18:57:35 +0200

    Added more features

--- todolist.py
+++ todolist.py
@@ -6,11 +6,7 @@ data = {'tasks': []}
 
 
 def add_task():
-    task_name = input("What is your task?")
-    priority = int(input("Priority of task from 1 to 3 (1 is low, 2 is medium, 3 is high)"))
-    if priority < 1 or priority > 3:
-        print("Priority must be from 1 to 3")
-        return
+    task_name = input("Type name of your task: ")
     data['tasks'].append({
         "task_name": task_name,
         "completed": False,
@@ -30,20 +26,45 @@ def show_tasks():
 
 
 def load_tasks_to_data_dictionary():
-    with open("test.json") as json_file:
-        data_json = json.load(json_file)
-        for t in data_json['tasks']:
-            data['tasks'].append({
-                "task_name": t['task_name'],
-                "completed": t['completed'],
-            })
+    try:
+        with open("test.json") as json_file:
+            data_json = json.load(json_file)
 
-    json_file.close()
+            for t in data_json['tasks']:
+                data['tasks'].append({
+                    "task_name": t['task_name'],
+                    "completed": t['completed'],
+                })
+
+        json_file.close()
+    except FileNotFoundError:
+        return
+
+
+def remove_task():
+    task_name_to_remove = input("Type task name that you want to remove")
+    filtered_data = filter(lambda task: task['task_name'] != task_name_to_remove, data['tasks'])
+    data['tasks'] = list(filtered_data)
+
+
+def mark_as_done():
+    task_idx = 0
+    for d in data['tasks']:
+        print(str(task_idx) + ". " + d['task_name'])
+        task_idx += 1
+
+    task_idx_input = int(input("Task number you want to mark as done: "))
+
+    if 0 <= task_idx_input <= len(data['tasks']) - 1:
+        data['tasks'][task_idx_input]['completed'] = True
+    else:
+        print("There is no task with index: " + str(task_idx_input))
+        return
 
 
 load_tasks_to_data_dictionary()
 
-while user_choice != 5:
+while user_choice != 6:
 
     if user_choice == 1:
         print()
@@ -52,18 +73,16 @@ while user_choice != 5:
         print()
         add_task()
     elif user_choice == 3:
-        # My problem
-        # mark_as_completed()
+        mark_as_done()
         print()
     elif user_choice == 4:
-        # My problem
-        # delete_task()
+        remove_task()
         print()
     elif user_choice == 5:
         print()
         save_tasks_to_file()
     else:
-        print("You have to choose number from 1 to 5")
+        print("You have to choose number from 1 to 6")
 
     print()
     print("1. Show tasks")
@@ -71,7 +90,7 @@ while user_choice != 5:
     print("3. Mark as done")
     print("4. Remove task")
     print("5. Save changes to file")
-    print("5. Exit")
+    print("6. Exit")
 
-    user_choice = int(input("Choose action to perform (number from 1 to 5): "))
+    user_choice = int(input("Choose action to perform (number from 1 to 6): "))
     print()

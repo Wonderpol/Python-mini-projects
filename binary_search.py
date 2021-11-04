20111629e091c08beba433f2443086dc4551305d
Jan Piaskowy <piaskowyjasiek@gmail.com>
2021-11-04 19:49:25 +0100

    Implementation of binary search

--- binary_search.py
+++ binary_search.py
@@ -6,3 +6,23 @@ def naive_search(list, target):
             return i
     return -1
 
+
+def binary_search(list, target, low=None, high=None):
+    if low is None:
+        low = 0
+    if high is None:
+        high = len(list) - 1
+
+    if high < low:
+        return -1
+
+    midpoint = (low + high) // 2
+
+    if list[midpoint] == target:
+        return midpoint
+    elif target < list[midpoint]:
+        return binary_search(list, target, low, midpoint - 1)
+    else:
+        # target > list[midpoint]
+        return binary_search(list, target, midpoint + 1, high)
+
